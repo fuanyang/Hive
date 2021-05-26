2abab3164fb1c5e5b3b282c6ab8f63f5fd46abb0
Fu An Yang <60484800+soda00000@users.noreply.github.com>
2021-05-25 20:14:19 -0500

    Update Glassdoor Employee Review Analysis.sql

--- Glassdoor Employee Review Analysis.sql
+++ Glassdoor Employee Review Analysis.sql
@@ -33,3 +33,26 @@ SELECT DISTINCT location,
   END AS country
 FROM review_tb2
 LIMIT 100;
+
+
+--6. Fill missing values in the table with default values
+
+CREATE TEMPORARY TABLE review_nonnull AS 
+SELECT index, company, location, dates,
+       REGEXP_REPLACE(OverallRatings, 'none', 4) nonull_OR,
+       REGEXP_REPLACE(WorkBalanceStars, 'none', 3.2) nonull_WB,
+       REGEXP_REPLACE(CultureStars, 'none', 4.1) nonull_CU,
+       REGEXP_REPLACE(CareerOpportunityStar, 'none', 5) nonull_CO,
+       REGEXP_REPLACE(BenefitStars, 'none', 4.7) nonull_BF
+FROM review_tb2;
+
+
+--7. Calculate median for all rating fields with newly added default values
+
+SELECT
+      ROUND(PERCENTILE_APPROX(CAST(nonull_OR as FLOAT), 0.5), 2) median_OR,
+      ROUND(PERCENTILE_APPROX(CAST(nonull_WB as FLOAT), 0.5), 2) median_WB,
+      ROUND(PERCENTILE_APPROX(CAST(nonull_CU as FLOAT), 0.5), 2) median_CS,
+      ROUND(PERCENTILE_APPROX(CAST(nonull_CO as FLOAT), 0.5), 2) median_CO,
+      ROUND(PERCENTILE_APPROX(CAST(nonull_BF as FLOAT), 0.5), 2) median_BF,
+FROM review_nonull;
